4a13d0fbc641e8dfb512aa03bd5caea109e7da2d
吴昱 <wuyu1989610@gmail.com>
2017-03-29 21:12:03 +0800

    implement edit action and destroy action

--- app/controllers/account/posts_controller.rb
+++ app/controllers/account/posts_controller.rb
@@ -4,4 +4,23 @@ class Account::PostsController < ApplicationController
   def index
     @posts = current_user.posts
   end
+
+  def edit
+    @post = Post.find(params[:group_id])
+  end
+
+  def update
+    @post = Post.find(params[:group_id])
+    if @post.update(post_params)
+      redirect_to account_posts_path, notice: "Update Success"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:group_id])
+    @post.destroy
+    redirect_to account_posts_path, alert: "Post deleted"
+  end
 end
